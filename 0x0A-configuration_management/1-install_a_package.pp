e06f8a82d1cd75f13c0068f22c70ecff5d718a2d
veekeey <meligavictoria@gmail.com>
2023-03-28 01:34:46 -0700

    first

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,20 @@
+# Install a package using Puppet
+
+exec { 'install flask':
+  command => '/usr/bin/apt-get install -y flask'
+}
+
+exec { 'install puppet-lint':
+  command => '/usr/bin/gem install puppet-lint -v 2.1.0'
+}
+
+package { 'flask':
+  ensure => 'installed',
+  before => Exec['install flask']
+}
+
+package { 'puppet-lint':
+  ensure  => 'installed',
+  before  => Exec['install puppet-lint'],
+  require => Package['flask']
+}
